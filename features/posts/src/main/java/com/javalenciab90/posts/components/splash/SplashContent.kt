16363e0016bceca0e9f3fee30affa415bfb77aa8
Jaime Valencia <javalenciab90@gmail.com>
2025-11-27 16:39:26 -0500

    Splash title

--- features/posts/src/main/java/com/javalenciab90/posts/components/splash/SplashContent.kt
+++ features/posts/src/main/java/com/javalenciab90/posts/components/splash/SplashContent.kt
@@ -6,6 +6,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SplashContent(
@@ -15,6 +19,12 @@ fun SplashContent(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text("The Posts")
+        Text(
+            text = "THE POSTS",
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 40.sp,
+            fontStyle = FontStyle.Italic,
+            color = Color.Black
+        )
     }
 }
